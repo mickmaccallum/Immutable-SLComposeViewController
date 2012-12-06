3f8b8760690fa7486e2433ff04a61df013affdf5
Michael MacCallum <Mick@Michaels-MacBook-Air.local>
2012-12-06 07:41:02 -0500

    Initial commit, all functionality added. This project allows you to prevent your users from making changes to pre formatted text that you pass to a SLComposeViewController. By default this project is configured to allow the user to make changes to the text at the beginning of the message but not to the link added to the end. If you would like to modify the project to prevent modification of any of the text within the message see README.md

---
+++ disableEditing/README.md
@@ -0,0 +1,43 @@
+SLComposeViewController editing prevention
+
+======================================
+
+I'm not going to write out a long description for this one. It's pretty self explanatory, this project demonstrates how to assign your view controller
+as the delegate of the UITextView inside the SLComposeViewController. Doing so allows us to check wether the range at which the user attempts to modify 
+the text is withing the "editable text" or the "permanent text" and return YES or NO accordingly.
+
+NOTE: If you wish to prevent the user from modifying ANY of the text, modify ```(BOOL)textView:shouldChangeTextInRange:replacementText``` to look like this:
+
+
+```objective-c- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    if (textView == sharingTextView) {
+        return NO;
+    }
+    return YES;
+}```
+
+
+
+Created by Michael MacCallum on 11/25/12.
+
+Accreditation is not required, but is always appreciated.
+
+This Software is provided on an "AS IS" basis.  I
+MAKE NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE, REGARDING THE SOFTWARE OR ITS USE AND
+OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+
+IN NO EVENT SHALL I BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+MODIFICATION AND/OR DISTRIBUTION OF THE SOFTWARE, HOWEVER CAUSED
+AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+STRICT LIABILITY OR OTHERWISE, EVEN IF I HAVE BEEN ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+That being said, you are free to use this code free of charge for absolutely
+anything you want. You may use this in personal projects, commercial projects
+or for anything else.
--- disableEditing/ViewController.m
+++ disableEditing/ViewController.m
@@ -3,27 +3,108 @@
 //  disableEditing
 //
 //  Created by Michael MacCallum on 12/5/12.
-//  Copyright (c) 2012 Michael MacCallum. All rights reserved.
+//  Accreditation is not required, but is always appreciated.
 //
-
+//  This Software is provided on an "AS IS" basis.  I
+//  MAKE NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+//  THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+//  FOR A PARTICULAR PURPOSE, REGARDING THE SOFTWARE OR ITS USE AND
+//  OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+//
+//  IN NO EVENT SHALL I BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+//  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+//  MODIFICATION AND/OR DISTRIBUTION OF THE SOFTWARE, HOWEVER CAUSED
+//  AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+//  STRICT LIABILITY OR OTHERWISE, EVEN IF I HAVE BEEN ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  That being said, you are free to use this code free of charge for absolutely
+//  anything you want. You may use this in personal projects, commercial projects
+//  or for anything else.
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <Social/Social.h>
+@interface ViewController () <UITextViewDelegate>
+{
+    UITextView *sharingTextView;
+}
 @end
-
 @implementation ViewController
 
-- (void)viewDidLoad
+
+
+- (NSString *)permanentText
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    return @"http://www.stackoverflow.com/"; //The user will not be able to modify this text.
 }
 
-- (void)didReceiveMemoryWarning
+- (NSString *)editableText
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return @"Hello, this is some awesome text I'm sharing with a permanent link!"; //This is the text the user will be able to edit
 }
+
+
+- (IBAction)exampleUsingFacebook:(UIButton *)sender {
+    
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+    {
+        SLComposeViewController *sharingComposer = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        
+        SLComposeViewControllerCompletionHandler __block completionHandler=^(SLComposeViewControllerResult result){
+            [sharingComposer dismissViewControllerAnimated:YES completion:nil];
+        };
+        [sharingComposer setCompletionHandler:completionHandler];
+        [sharingComposer setInitialText:[NSString stringWithFormat:@"%@ %@",[self editableText],[self permanentText]]];
+        
+        [self presentViewController:sharingComposer animated:YES completion:^{
+            for (UIView *viewLayer1 in [[sharingComposer view] subviews]) {
+                for (UIView *viewLayer2 in [viewLayer1 subviews]) {
+                    if ([viewLayer2 isKindOfClass:[UIView class]]) {
+                        for (UIView *viewLayer3 in [viewLayer2 subviews]) {
+                            if ([viewLayer3 isKindOfClass:[UITextView class]]) {
+                                [(UITextView *)viewLayer3 setDelegate:self];
+                                sharingTextView = (UITextView *)viewLayer3;
+                            }
+                        }
+                    }
+                }
+            }
+        }];
+    }
+}
+
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    if (textView == sharingTextView) {
+        NSRange substringRange = [[textView text] rangeOfString:[self permanentText]];
+        if (range.location >= substringRange.location && range.location <= substringRange.location + substringRange.length) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
